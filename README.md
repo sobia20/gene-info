8fd7c2bb9333586f322246aa0476a11169093c3f
Sobia Khalid <sobia.khalid.2126@gmail.com>
2023-10-01 19:16:09 +0500

    get api call, docker compose file

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 ## Gene Information
 
 This FAST API application allows you to get the gene information from a mysql database. 
+
+Visit http://localhost:8000/docs  to access the api to get gene information using the gene symbol.
+
+![screen_gif](https://imgur.com/0UZTMLv)
+
+### Getting started
+To clone, 
+```
+git clone 
+cd clone
+```
+
+To start the application, 
+```
+docker-compose up -d
+```
+
+Please visit http://localhost:8000/docs to use the api. 
+
+To shutdown the application, 
+```
+docker-compose down
+```
--- gene-info/gene_info/main.py
+++ gene-info/gene_info/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 from gene_info import models
 from gene_info.database import SessionLocal, engine
@@ -20,5 +20,34 @@ def startup_populate_db():
         populate_db(db)
     finally:
         db.close()
+
+
+@app.get('/gene')
+def get_genes_by_symbol(gene_symbol: str, db: Session = Depends(get_db)):
+    query = (
+        db.query(
+            models.Gene.symbol.label('gene_symbol'),
+            models.Gene.stable_id.label('gene_stable_id'),
+            models.Transcript.stable_id.label('transcript_stable_id')
+        )
+        .join(models.Transcript, models.Gene.gene_id == models.Transcript.gene_id)
+        .filter(models.Gene.symbol == gene_symbol)
+    )
+
+    results = query.all()
+
+    gene_data = {
+        'gene_symbol': gene_symbol,
+        'genes': [
+            {
+                'gene_stable_id': result.gene_stable_id,
+                'transcript_stable_id': result.transcript_stable_id,
+            }
+            for result in results
+        ]
+    }
+
+    return gene_data
+
 
 startup_populate_db()
